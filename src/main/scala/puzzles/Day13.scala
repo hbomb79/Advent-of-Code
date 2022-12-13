42ac007428c352ccc072dbe77695b08e74b40c2a
Harry Felton <harryfelton12@gmail.com>
2022-12-13 22:11:34 +1300

    Day 13 cleanup

--- src/main/scala/puzzles/Day13.scala
+++ src/main/scala/puzzles/Day13.scala
@@ -1,140 +1,115 @@
 package puzzles
 
 import main.Puzzle
-import scala.util.matching.Regex
-import scala.compiletime.ops.boolean
 
 object PuzzleThirteen extends Puzzle {
   override def partOne(fileLines: Seq[String]): Unit = {
-    println(compare(parse(fileLines)).sum)
+    println(findOrderedPairs(parse(fileLines)).sum)
   }
   override def partTwo(fileLines: Seq[String]): Unit = {
-    val lines = parse(fileLines).flatMap(a => Seq(a._1, a._2))
-    val dividers = Seq(
-      ContentList(Seq(ContentList(Seq(2)))),
-      ContentList(Seq(ContentList(Seq(6))))
-    )
+    import ContentList.wrap // :eyes:
 
-    val sorted = sort(lines ++ dividers)
+    val flattenedLines = parse(fileLines).flatMap(a => Seq(a._1, a._2))
+    val dividers = Seq(2, 6).map(x => wrap(wrap(x)))
+
+    val sorted = sort(flattenedLines ++ dividers)
     val dividerIndex = dividers
-      .map(d => sorted.indexOf(d) + 1)
+      .map(dIndex => sorted.indexOf(dIndex) + 1)
       .reduce(_ * _)
     println(dividerIndex)
   }
 
   private def sort(content: Seq[Content]): Seq[Content] =
     content.sortWith((a, b) =>
-      compareLists(Seq(a), Seq(b)) match {
-        case Result.Ordered   => true
-        case Result.Unordered => false
-        case Result.Unknown =>
-          throw new IllegalStateException(
-            "Comparison of top-level list was unconclusive"
-          )
-          false
-      }
+      compareLists(Seq(a), Seq(b)) == ComparisonResult.Ordered
     )
 
-  private def compare(pairs: Seq[ListPairs]): Seq[Int] = {
+  private def findOrderedPairs(pairs: Seq[ContentPair]): Seq[Int] = {
     pairs.zipWithIndex.collect {
       case ((ContentList(l), ContentList(r)), index) =>
-        compareLists(l, r) match {
-          case Result.Ordered   => Some(index + 1)
-          case Result.Unordered => None
-          case Result.Unknown =>
-            throw new IllegalStateException(
-              "Comparison of top-level list was unconclusive"
-            )
-            None
-        }
+        compareLists(l, r) match
+          case ComparisonResult.Ordered => Some(index + 1)
+          case _                        => None
     }.flatten
   }
 
   private def compareLists(
       left: Seq[Content],
       right: Seq[Content]
-  ): Result =
-    (left, right) match {
-      case (Nil, Nil) => Result.Unknown
-      case (Nil, _)   => Result.Ordered
-      case (_, Nil)   => Result.Unordered
-      case (lH :: lRest, rH :: rRest) =>
-        (lH, rH) match {
-          case (ContentList(l), ContentList(r)) =>
-            // Both items are lists... Compare each value
-            compareLists(l, r) match {
-              case Result.Unknown =>
-                compareLists(lRest, rRest)
-              case r => r
-            }
-          case (l: Int, r: Int) =>
-            // Both items are Ints... compare
-            if (l < r) {
-              Result.Ordered
-            } else if (r < l) {
-              Result.Unordered
-            } else {
-              // Not sure.. need to keep looking
-              compareLists(lRest, rRest)
-            }
-          case (l: Int, r @ ContentList(_)) =>
-            // Left side is an int, but right side is list.. convert
-            compareLists(ContentList(Seq(l)) +: lRest, right)
-          case (l @ ContentList(_), r: Int) =>
-            // Left side is a list, but right side is an int .. convert
-            compareLists(left, ContentList(Seq(rH)) +: rRest)
-        }
-    }
+  ): ComparisonResult = (left, right) match {
+    case (Nil, Nil) => ComparisonResult.Unknown
+    case (Nil, _)   => ComparisonResult.Ordered
+    case (_, Nil)   => ComparisonResult.Unordered
+    case (leftHead :: leftRest, rightHead :: rightRest) =>
+      (leftHead, rightHead) match {
+        case (l: Int, r: Int) if l < r    => ComparisonResult.Ordered
+        case (l: Int, r: Int) if r < l    => ComparisonResult.Unordered
+        case (_: Int, _: Int)             => compareLists(leftRest, rightRest)
+        case (_: Int, r @ ContentList(_)) =>
+          // Type mismatch. Box leftHead in a ContentList and retry
+          compareLists(ContentList(Seq(leftHead)) +: leftRest, right)
+        case (l @ ContentList(_), _: Int) =>
+          // Type mismatch. Box rightHead in a ContentList and retry
+          compareLists(left, ContentList(Seq(rightHead)) +: rightRest)
+        case (ContentList(l), ContentList(r)) =>
+          compareLists(l, r) match
+            case ComparisonResult.Unknown => compareLists(leftRest, rightRest)
+            case r                        => r
+      }
+  }
 
-  private def parse(
-      lines: Seq[String]
-  ): Seq[ListPairs] = lines
+  private def parse(lines: Seq[String]): Seq[ContentPair] = lines
     .grouped(3)
     .map { case (first :: second :: _) =>
-      val f = constructContent(first, Seq.empty, None).head
-      val s = constructContent(second, Seq.empty, None).head
-      (f, s)
+      (
+        constructContent(first, Seq.empty, None).head,
+        constructContent(second, Seq.empty, None).head
+      )
     }
     .toSeq
 
   private def constructContent(
       line: String,
       acc: Seq[Content],
-      bracketAcc: Option[String]
-  ): Seq[Content] = {
-    if (line.isEmpty) {
-      acc
-    } else {
-      val head = line.charAt(0)
-      val rest = line.drop(1)
-      head match {
-        case '[' if bracketAcc.isEmpty =>
-          constructContent(rest, acc, Some(""))
-        case ']'
-            if bracketAcc.nonEmpty &&
-              bracketAcc
-                .map(_.count(_ == '[')) == bracketAcc.map(_.count(_ == ']')) =>
-          val bracketVal = constructContent(bracketAcc.get, Seq.empty, None)
-          constructContent(rest, acc :+ ContentList(bracketVal), None)
-        case c if bracketAcc.nonEmpty =>
-          constructContent(rest, acc, bracketAcc.map(_ :+ c))
+      nestedAcc: Option[String]
+  ): Seq[Content] =
+    if (line.isEmpty) acc
+    else {
+      val tail = line.drop(1)
+      val canCloseNested =
+        nestedAcc.map(n => n.count(_ == '[') == n.count(_ == ']'))
+
+      line.head match {
+        // Nested handling
+        case '[' if nestedAcc.isEmpty =>
+          constructContent(tail, acc, Some(""))
+        case ']' if canCloseNested == Some(true) =>
+          val nestedValue = constructContent(nestedAcc.get, Seq.empty, None)
+          constructContent(tail, acc :+ ContentList(nestedValue), None)
+        case c if nestedAcc.nonEmpty =>
+          constructContent(tail, acc, nestedAcc.map(_ :+ c))
+
+        // Top level handling
         case ',' =>
-          constructContent(rest, acc, bracketAcc)
+          constructContent(tail, acc, nestedAcc)
         case _ =>
           val substr = line.takeWhile(_.isDigit)
           constructContent(
             line.drop(substr.length),
             acc :+ substr.toInt,
-            bracketAcc
+            nestedAcc
           )
       }
     }
-  }
 
-  private enum Result:
+  private enum ComparisonResult:
     case Ordered, Unordered, Unknown
 
-  type ListPairs = (Content, Content)
-  type Content = ContentList | Int
-  case class ContentList(contents: Seq[Content])
+  private type Content = ContentList | Int
+  private type ContentPair = (Content, Content)
+
+  private case class ContentList(contents: Seq[Content])
+  private object ContentList {
+    def wrap = (c: Content) => ContentList(Seq(c))
+  }
 }
