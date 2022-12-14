f53d85d15630d573760a7b362d2d88e14320acd5
Harry Felton <harryfelton12@gmail.com>
2022-12-14 22:45:07 +1300

    Day 14 cleanup

--- src/main/scala/puzzles/Day14.scala
+++ src/main/scala/puzzles/Day14.scala
@@ -4,102 +4,113 @@ import main.Puzzle
 import scala.annotation.tailrec
 
 object PuzzleFourteen extends Puzzle {
-    override def partOne(fileLines: Seq[String]): Unit = {
-        //TODO
-    }
-    override def partTwo(fileLines: Seq[String]): Unit = {
-        val l = parse(fileLines)
-        println(l)
+  val SandEntryPoint = Point(500, 0)
+  override def partOne(fileLines: Seq[String]): Unit = {
+    val lines = parse(fileLines)
+    val maximumY = lines.map(_.y).max
 
-        println
-        val n = runSim(Point(500, 0), l, Set.empty)
-        println(n.size)
-    }
+    val restingSand = runSimulation(SandEntryPoint, lines, Set.empty, maximumY)(
+      completionPredicate = _.isEmpty,
+      moveFilter = _ => true
+    )
 
-    private def parse(lines: Seq[String]): Set[Point] =
-        lines.foldLeft(Set.empty[Point]) { case (blockedPoints, line) =>
-            blockedPoints ++ parseLine(extractPoints(line, Seq.empty), blockedPoints)
-        }
+    println(restingSand.size)
+  }
+  override def partTwo(fileLines: Seq[String]): Unit = {
+    val lines = parse(fileLines)
+    val maximumY = lines.map(_.y).max + 2
 
-    private def runSim(entryPoint: Point, wallPoints: Set[Point], sandPoints: Set[Point]): Set[Point] = {
-        if(sandPoints.size % 100 == 0) {
-            println(s"Simulating sand #${sandPoints.size}")
-        }
-        simulateSand(entryPoint, wallPoints, sandPoints) match {
-            case Some(restPoint) if restPoint.x == entryPoint.x && restPoint.y == entryPoint.y =>
-                sandPoints + restPoint
-            case Some(restPoint) => runSim(entryPoint, wallPoints, sandPoints + restPoint)
-            case None =>
-                throw new IllegalStateException()
-        }
-    }
+    val restingSand = runSimulation(SandEntryPoint, lines, Set.empty, maximumY)(
+      completionPredicate = _.contains(SandEntryPoint),
+      moveFilter = _.y < maximumY
+    )
 
-    @tailrec
-    private def simulateSand(fallPoint: Point, blockedPoints: Set[Point], sandPoints: Set[Point]): Option[Point] = {
-        // Test if fallPoint can move down, down left, or down right. If blocked by sand, or wall
-        // will come to rest and return true. Otherwise, move the point and recurse
-        val lowestY = blockedPoints.map(_.y).max + 2
-        val down = fallPoint.copy(y = fallPoint.y + 1)
-        val attemptMoves = Seq(down, down.copy(x = down.x-1), down.copy(x = down.x+1)).filter(_.y < lowestY)
-        attemptMoves.find(a => !sandPoints.contains(a) && !blockedPoints.contains(a)) match {
-            case None => 
-                // No valid moves, all are blocked. Comes to a rest
-                Some(fallPoint)
-            case Some(Point(_, y)) if y > lowestY =>
-                // Falling out of world 
-                None
-            case Some(point) =>
-                // Move down
-                simulateSand(point, blockedPoints, sandPoints)
-        }
-    }
+    println(restingSand.size)
+  }
 
-    sealed case class Point(x: Int, y: Int)
-    private def extractPoints(line: String, points: Seq[Point]): Seq[Point] = {
-        val l = line.trim
-        l match {
-            case s if s.isEmpty => points
-            case s if s.startsWith("->") =>
-                // Start of arrow, consume
-                extractPoints(s.drop(2), points)
-            case _ =>
-                // Should be a number pair, extract
-                val first = l.takeWhile(_.isDigit)
-                val second = l.drop(first.length + 1).takeWhile(_.isDigit)
-                val rest = l.drop(first.length + second.length + 2)
-                extractPoints(rest, points :+ Point(first.toInt,second.toInt))
-        }
+  @tailrec
+  private def runSimulation(
+      entryPoint: Point,
+      wallPoints: Set[Point],
+      existingSand: Set[Point],
+      maximumY: Int
+  )(implicit
+      completionPredicate: Option[Point] => Boolean,
+      moveFilter: Point => Boolean
+  ): Set[Point] =
+    simulateSand(
+      entryPoint,
+      wallPoints,
+      existingSand,
+      moveFilter,
+      maximumY
+    ) match {
+      case p if completionPredicate(p) => existingSand ++ Seq(p).flatten
+      case Some(restPoint) =>
+        runSimulation(
+          entryPoint,
+          wallPoints,
+          existingSand + restPoint,
+          maximumY
+        )
+      case _ => throw new IllegalStateException("Unexpected simulation result")
     }
 
-    private def parseLine(points: Seq[Point], blocked: Set[Point]): Set[Point] = 
-        points.sliding(2).foldLeft(Set.empty[Point]) {
-            case (acc, Seq(_)) =>
-                // Last element with no connecting point.
-                acc
-            case (acc, Seq(first, second)) =>
-                if(first.x == second.x) {
-                    // Moving along the Y axis (vertical line)
-                    val ps = Seq(first.y, second.y)
-                    val min = ps.min
-                    val max = ps.max
+  @tailrec
+  private def simulateSand(
+      fallPoint: Point,
+      blockedPoints: Set[Point],
+      sandPoints: Set[Point],
+      moveFilter: Point => Boolean,
+      maximumY: Int
+  ): Option[Point] = Seq(0, -1, 1)
+    .map(dX => fallPoint.copy(x = fallPoint.x + dX, y = fallPoint.y + 1))
+    .filter(moveFilter)
+    .find(a => !sandPoints.contains(a) && !blockedPoints.contains(a))
+    .match {
+      case None                              => Some(fallPoint)
+      case Some(Point(_, y)) if y > maximumY => None
+      case Some(point) =>
+        simulateSand(point, blockedPoints, sandPoints, moveFilter, maximumY)
+    }
 
-                    val n = (min to max).map{ y => 
-                        Point(first.x, y)
-                    }
+  private def parse(lines: Seq[String]): Set[Point] =
+    lines.foldLeft(Set.empty[Point])((blockedPoints, line) =>
+      formLines(extractPoints(line, Seq.empty), blockedPoints)
+    )
 
-                    acc ++ n
-                } else {
-                    // Moving along the X axis (horizontal line)
-                    val ps = Seq(first.x, second.x)
-                    val min = ps.min
-                    val max = ps.max
+  @tailrec
+  private def extractPoints(line: String, points: Seq[Point]): Seq[Point] =
+    line match {
+      case s if s.isEmpty => points
+      case s if s.startsWith("->") =>
+        extractPoints(s.drop(3), points)
+      case _ =>
+        val first = line.takeWhile(_.isDigit)
+        val second = line.drop(first.length + 1).takeWhile(_.isDigit)
+        val rest = line.drop(first.length + second.length + 2)
 
-                    val n = (min to max).map{ x => 
-                        Point(x, first.y)
-                    }
+        extractPoints(rest, points :+ Point(first.toInt, second.toInt))
+    }
 
-                    acc ++ n
-                }
+  @tailrec
+  private def formLines(
+      lineVertices: Seq[Point],
+      filledPoints: Set[Point]
+  ): Set[Point] = lineVertices match {
+    case _ +: Nil =>
+      filledPoints
+    case first +: second +: rest =>
+      val n = if (first.x == second.x) {
+        val ps = Seq(first.y, second.y)
+        (ps.min to ps.max).map(y => Point(first.x, y))
+      } else {
+        val ps = Seq(first.x, second.x)
+        (ps.min to ps.max).map(x => Point(x, first.y))
+      }
 
-        }
+      formLines(second +: rest, filledPoints ++ n)
+  }
+
+  sealed case class Point(x: Int, y: Int)
 }
