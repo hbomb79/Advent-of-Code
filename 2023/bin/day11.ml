83f64882b191c36ece799fc69548fd538b6c74c6
Harry Felton <harryfelton12@gmail.com>
2023-12-13 17:19:41 +1300

    Moved IntTuple to Advent lib

--- 2023/bin/day11.ml
+++ 2023/bin/day11.ml
@@ -1,17 +1,6 @@
 open Core
 module IntSet = CCSet.Make (Int)
-
-module IntTuple = struct
-  type t = int * int
-
-  let compare (x1, y1) (x2, y2) =
-    match compare x1 x2 with
-    | 0 -> compare y1 y2
-    | c -> c
-  ;;
-end
-
-module IntTupleSet = CCSet.Make (IntTuple)
+module IntTupleSet = CCSet.Make (Advent.IntTuple)
 
 let read_char x y ch =
   match ch with
---
+++ 2023/lib/intTuple.ml
@@ -0,0 +1,7 @@
+type t = int * int
+
+let compare (x1, y1) (x2, y2) =
+  match compare x1 x2 with
+  | 0 -> compare y1 y2
+  | c -> c
+;;
