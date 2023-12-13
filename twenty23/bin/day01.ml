4603dd29762fa31cab5343c1607b93a7875828c2
Harry Felton <harryfelton12@gmail.com>
2023-12-13 17:19:40 +1300

    initial OCaml project structure

---
+++ twenty23/bin/day01.ml
@@ -0,0 +1,6 @@
+
+(* Part one *)
+let () =
+    let input = Advent.read_lines("./inputs/day01-test.txt") in
+    let _ = List.map (fun line -> print_endline line) input in
+    ()
---
+++ twenty23/lib/advent.ml
@@ -0,0 +1,8 @@
+open Core
+
+let read_lines file =
+  Stdio.In_channel.with_file file ~f:(fun channel ->
+    let x = In_channel.input_all channel in
+    String.split_lines x)
+;;
+
