86ea2f3c090d4c5487adc915a392f5e41762ea77
Harry Felton <harryfelton12@gmail.com>
2023-12-19 20:47:06 +1300

    Day 17 re-solved using A Star

--- 2023/bin/day17.ml
+++ 2023/bin/day17.ml
@@ -8,6 +8,8 @@ module Node = struct
   let pos (x, y, _, _) = x, y
 end
 
+module AStar = Advent.Pathfinding.AStar (Node)
+
 (* Given the grid and the maximum allowed momentum (the number of cells
    a node is allowed to move in one direction before it must turn), this function
    will return a list of valid neighboring nodes *)
@@ -36,35 +38,22 @@ let adj_nodes grid min_momentum max_momentum (x, y, dir, momentum) =
 ;;
 
 let g_of_node grid (x, y, _, _) = ArrayGrid.point_at_xy grid (x, y) |> Option.value_exn
+let h_of_node (tx, ty) (x, y, _, _) = abs (tx - x) + abs (ty - y)
 
-let is_goal target minm (_, (x, y, _, momentum)) =
+let is_goal target minm (x, y, _, momentum) =
   Point.equal (x, y) target && momentum >= minm
 ;;
-
-module Djikstra = Advent.Pathfinding.Djikstra (Node)
 
 let _ =
   let lines = Advent.Strings.read_lines "./inputs/day17.txt" in
   let grid = ArrayGrid.from_lines lines ~f:(fun ch -> int_of_string (String.make 1 ch)) in
   let start_seeds = [ 0, 0, Direction.East, 1; 0, 0, Direction.South, 1 ] in
   let target = ArrayGrid.width grid - 1, ArrayGrid.height grid - 1 in
-  let part_one_hl, _ =
-    Djikstra.find_shortest_path
-      grid
-      start_seeds
-      ~goal:(is_goal target 0)
-      ~adj:(adj_nodes grid 0 3)
-      ~g:(g_of_node grid)
+  let find_path =
+    AStar.find_shortest_path grid start_seeds ~h:(h_of_node target) ~g:(g_of_node grid)
   in
-  let _ = Printf.printf "Part One: %d\n" part_one_hl in
-  let part_two_hl, _ =
-    Djikstra.find_shortest_path
-      grid
-      start_seeds
-      ~goal:(is_goal target 4)
-      ~adj:(adj_nodes grid 4 10)
-      ~g:(g_of_node grid)
-  in
-  let _ = Printf.printf "Part Two: %d\n" part_two_hl in
+  let part_one_hl, _ = find_path ~goal:(is_goal target 0) ~adj:(adj_nodes grid 0 3) in
+  let part_two_hl, _ = find_path ~goal:(is_goal target 4) ~adj:(adj_nodes grid 4 10) in
+  let _ = Printf.printf "Part One: %d\nPart Two: %d\n" part_one_hl part_two_hl in
   ()
 ;;
--- 2023/lib/pathfinding.ml
+++ 2023/lib/pathfinding.ml
@@ -50,3 +50,66 @@ module Djikstra (Node : CCMap.OrderedType) = struct
     aux (PQ.of_list start) (S.of_list []) (M.of_list [])
   ;;
 end
+
+module AStar (Node : CCMap.OrderedType) = struct
+  (* A graph node 'Node' tupled with it's accumulated cost to get to that node [used to
+   sort priority queue] *)
+  module NodeWithCost = struct
+    type t =
+      { gcost : int
+      ; hcost : int
+      ; state : Node.t
+      }
+
+    let fcost { gcost; hcost; _ } = gcost + hcost
+    let compare a b = compare (fcost a) (fcost b)
+  end
+
+  module PQ = CCHeap.Make_from_compare (NodeWithCost)
+  module S = CCSet.Make (Node)
+  module M = CCMap.Make (Node)
+
+  let construct_path_to node tracks =
+    let rec aux acc current =
+      match M.get current tracks with
+      | None -> acc
+      | Some parent -> aux (parent :: acc) parent
+    in
+    aux [ node ] node
+  ;;
+
+  let find_shortest_path _grid seeds ~adj ~goal ~g ~h =
+    let open NodeWithCost in
+    let rec aux open_nodes closed_nodes parents =
+      match PQ.take open_nodes with
+      | None -> failwith "no path to target"
+      | Some (_, { gcost; state; _ }) when goal state ->
+        gcost, construct_path_to state parents
+      | Some (open_nodes', { state; _ }) when S.mem state closed_nodes ->
+        aux open_nodes' closed_nodes parents
+      | Some (open_nodes', { gcost; state; _ }) ->
+        let closed_nodes' = S.add state closed_nodes in
+        let neighbors =
+          adj state |> List.filter ~f:(fun n -> not (S.mem n closed_nodes))
+        in
+        let open_nodes'', tracks' =
+          List.fold_left
+            ~init:(open_nodes', parents)
+            ~f:(fun (onodes, ts) neighbor ->
+              let onodes' =
+                PQ.add
+                  onodes
+                  { gcost = gcost + g neighbor; hcost = h neighbor; state = neighbor }
+              in
+              let ts' = M.add neighbor state ts in
+              onodes', ts')
+            neighbors
+        in
+        aux open_nodes'' closed_nodes' tracks'
+    in
+    let start =
+      List.map ~f:(fun seed -> { gcost = 0; hcost = h seed; state = seed }) seeds
+    in
+    aux (PQ.of_list start) (S.of_list []) (M.of_list [])
+  ;;
+end
