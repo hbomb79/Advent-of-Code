87b34bd9889c372d38a85760737ca303c4554776
Harry Felton <harryfelton12@gmail.com>
2024-12-03 18:29:22 +1300

    Day 03

---
+++ 2024/lib/puzzles/day03.ex
@@ -0,0 +1,18 @@
+defmodule Puzzles.Day03 do
+  def part1(input), do: solve(input, false)
+  def part2(input), do: solve(input, true)
+
+  def solve(input, expanded) do
+    # Pluck all matches for `mul(X,Y)`, `don't` and `do`.
+    ~r/(?:don't)|(?:do)|mul\((\d{1,3}),(\d{1,3})\)/
+    |> Regex.scan(input)
+    |> List.foldl({0, true}, fn
+      ["do"], {acc, _} -> {acc, true}
+      ["don't"], {acc, _} when expanded -> {acc, false}
+      ["don't"], {acc, _} when not expanded -> {acc, true}
+      _, {acc, false} -> {acc, false}
+      [_, x, y], {acc, true} -> {acc + String.to_integer(x) * String.to_integer(y), true}
+    end)
+    |> elem(0)
+  end
+end
