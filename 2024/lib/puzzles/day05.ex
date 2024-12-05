3730c8098cc2f47148db9f034108a4e1332e1d36
Harry Felton <harryfelton12@gmail.com>
2024-12-06 09:46:46 +1300

    Improve Day05

--- 2024/lib/puzzles/day05.ex
+++ 2024/lib/puzzles/day05.ex
@@ -1,45 +1,21 @@
 defmodule Puzzles.Day05 do
   def part1(input) do
-    {valid, _, _} = parse(input)
-
-    Enum.reduce(valid, 0, fn upd, acc ->
-      acc + String.to_integer(Enum.at(upd, floor(length(upd) / 2)))
-    end)
+    parse(input) |> elem(0) |> Enum.reduce(0, &(&2 + at_middle(&1)))
   end
 
   def part2(input) do
-    {_, invalid, comes_before} = parse(input)
-
-    Enum.map(invalid, fn upd ->
-      Enum.sort(upd, fn a, b ->
-        case {a in Map.get(comes_before, b, []), b in Map.get(comes_before, a, [])} do
-          {true, false} -> false
-          _ -> true
-        end
-      end)
-    end)
-    |> Enum.reduce(0, fn upd, acc ->
-      acc + String.to_integer(Enum.at(upd, floor(length(upd) / 2)))
-    end)
+    parse(input) |> elem(1) |> Enum.reduce(0, &(&2 + at_middle(&1)))
   end
 
-  defp is_correct([], _, _), do: true
+  defp at_middle(list), do: String.to_integer(Enum.at(list, floor(length(list) / 2)))
 
-  defp is_correct([hd | tl], must_come_before, seen) do
-    case Map.fetch(must_come_before, hd) do
-      :error ->
-        # Nothing must come before this update
-        is_correct(tl, must_come_before, MapSet.put(seen, hd))
-
-      {:ok, vals} ->
-        if Enum.all?(vals, &MapSet.member?(seen, &1)) do
-          # If everything in vals is something we've seen already, all good
-          is_correct(tl, must_come_before, MapSet.put(seen, hd))
-        else
-          # one or more items must come before this node
-          false
-        end
-    end
+  defp sort_update(update, comes_before) do
+    Enum.sort(update, fn a, b ->
+      case {a in Map.get(comes_before, b, []), b in Map.get(comes_before, a, [])} do
+        {true, false} -> true
+        _ -> false
+      end
+    end)
   end
 
   defp parse(input) do
@@ -56,18 +32,17 @@ defmodule Puzzles.Day05 do
         end
       end)
 
-    {valid, invalid} =
-      Input.parse_lines(updates)
-      |> Enum.map(fn input -> String.split(input, ",", trim: true) end)
-      |> split(comes_before)
-
-    {valid, invalid, comes_before}
+    Input.parse_lines(updates)
+    |> Enum.map(fn input -> String.split(input, ",", trim: true) end)
+    |> split(comes_before)
   end
 
   defp split(updates, comes_before) do
-    Enum.split_with(updates, fn upd ->
-      deps = Map.new(comes_before, fn {k, v} -> {k, Enum.filter(v, fn vv -> vv in upd end)} end)
-      is_correct(upd, deps, MapSet.new())
+    Enum.reduce(updates, {[], []}, fn upd, {valid, invalid} ->
+      case sort_update(upd, comes_before) do
+        ^upd -> {[upd | valid], invalid}
+        diff -> {valid, [diff | invalid]}
+      end
     end)
   end
 end
