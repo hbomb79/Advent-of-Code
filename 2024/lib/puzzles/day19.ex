4d972c22b410033163b29359d977fa79eb52e687
Harry Felton <harryfelton12@gmail.com>
2024-12-19 19:11:17 +1300

    Day 19 tidy

--- 2024/lib/puzzles/day19.ex
+++ 2024/lib/puzzles/day19.ex
@@ -1,10 +1,17 @@
 defmodule Puzzles.Day19 do
   def part1(input) do
     {patterns, towels} = parse(input)
-    Enum.count(towels, &(combinations(&1, patterns, %{}) |> elem(0) != 0))
+    Enum.count(towels, &(get_combinations(&1, patterns) != 0))
   end
 
-  defp combinations("", _patterns, cache), do: {1, cache}
+  def part2(input) do
+    {patterns, towels} = parse(input)
+    Enum.reduce(towels, 0, &(&2 + get_combinations(&1, patterns)))
+  end
+
+  defp get_combinations(towel, patterns), do: combinations(towel, patterns, %{}) |> elem(0)
+
+  defp combinations("", _, cache), do: {1, cache}
 
   defp combinations(towel, patterns, cache) do
     case Map.fetch(cache, towel) do
@@ -17,17 +24,9 @@ defmodule Puzzles.Day19 do
           {acc + res, Map.put(new_cache, towel, res)}
         end)
 
-      {:ok, v} ->
-        {v, cache}
+      {:ok, hit} ->
+        {hit, cache}
     end
-  end
-
-  def part2(input) do
-    {patterns, towels} = parse(input)
-
-    Enum.reduce(towels, 0, fn towel, acc ->
-      acc + (combinations(towel, patterns, %{}) |> elem(0))
-    end)
   end
 
   defp parse(input) do
